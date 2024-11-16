6fe3b8e55e7ce20411811d5836423501a3277802
kyw04 <85551017+kyw04@users.noreply.github.com>
2024-11-16 05:36:19 +0000

    add item(wood bow)

---
+++ Header/Item/WoodBow.hpp
@@ -0,0 +1,22 @@
+#ifndef _WOOD_BOW_
+#define _WOOD_BOW_
+#include "Item.hpp"
+
+class WoodSword : public Item
+{
+public:
+    WoodSword() : Item(3)
+    {
+        state = ItemState::Unequipped;
+        count = 0;
+        level_requirement = 1;
+        job_requirement = EntityJob::Archer;
+
+        category = ItemCategory::Weapon;
+        name = "나무 활";
+        rank = ItemRank::Normal;
+        stats = new Stats(0, 0, 0, 0, 7, 0, 3, 3);
+    }
+};
+
+#endif
