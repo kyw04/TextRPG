a6d48541421eb6861bc9577411e48127763731d2
kyw04 <85551017+kyw04@users.noreply.github.com>
2024-11-18 05:56:03 +0000

    update tile probability

--- Header/Map.hpp
+++ Header/Map.hpp
@@ -11,8 +11,8 @@ class Map
 private:
     const std::vector<std::pair<TileState, double>> tile_probability =
     {
-        { TileState::Wall, 0.4 },
-        { TileState::Entity, 0.3 },
+        { TileState::Wall, 0.3 },
+        { TileState::Entity, 0.4 },
         { TileState::Treasure, 0.05 },
         { TileState::Trap, 0.05 }
     };
