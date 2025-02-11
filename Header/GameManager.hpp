528274e94d4231a8913dd019fec170aa565d53d3
kyw04 <85551017+kyw04@users.noreply.github.com>
2025-02-11 13:54:52 +0000

    update: Treasure item update

--- Header/GameManager.hpp
+++ Header/GameManager.hpp
@@ -50,13 +50,13 @@ private:
     Random<Item*> random_item_data = std::vector<RandomItem<Item*>>
     (
         {
-            RandomItem<Item*>(new WoodBow(), 0.01, true),
-            RandomItem<Item*>(new WoodSword(), 0.01, true),
-            RandomItem<Item*>(new WoodStaff(), 0.01, true),
-            RandomItem<Item*>(new LeatherHelmet(), 0.01, true),
-            RandomItem<Item*>(new LeatherArmor(), 0.01, true),
-            RandomItem<Item*>(new LeatherLeggings(), 0.01, true),
-            RandomItem<Item*>(new LeatherShoes(), 0.01, true),
+            RandomItem<Item*>(new WoodBow(), 0.01),
+            RandomItem<Item*>(new WoodSword(), 0.01),
+            RandomItem<Item*>(new WoodStaff(), 0.01),
+            RandomItem<Item*>(new LeatherHelmet(), 0.01),
+            RandomItem<Item*>(new LeatherArmor(), 0.01),
+            RandomItem<Item*>(new LeatherLeggings(), 0.01),
+            RandomItem<Item*>(new LeatherShoes(), 0.01),
         }
     );
 
